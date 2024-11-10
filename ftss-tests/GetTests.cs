a7dbdcc5c5240c7335e8ae4230aa1cc9632d8250
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2024-11-10 12:02:16 -0500

    Get() tests.

---
+++ ftss-tests/GetTests.cs
@@ -0,0 +1,52 @@
+﻿using ftss;
+
+namespace ftss_tests;
+[TestClass]
+public class GetTests
+{
+    [TestMethod]
+    public void EmptySet()
+    {
+        // Arrange
+        FastTernaryStringSet test = [];
+
+        // Act & Assert
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => test.Get(-1), "Test A");
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => test.Get(0), "Test B");
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => test.Get(1), "Test C");
+    }
+
+    [TestMethod]
+    public void EmptyString()
+    {
+        // Arrange
+        FastTernaryStringSet test = [];
+        test.Add(String.Empty);
+
+        // Act
+        string s = test.Get(0);
+
+        // Assert
+        Assert.AreEqual(s, string.Empty);
+    }
+
+    [TestMethod]
+    public async Task GetRandomWords()
+    {
+        // Arrange
+        FastTernaryStringSet test = [];
+        string[] lines = (await Common.GetResourceFileContents("short-english-list.txt"))
+            .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        test.AddAll(lines);
+
+        // Act & Assert
+        Console.WriteLine("Getting random words from short word list.");
+        for (int i = 0; i < 100;  i++)
+        {
+            int index = Random.Shared.Next(0, lines.Length - 1);
+            string word = test.Get(index);
+            Console.WriteLine(word);
+            Assert.AreNotEqual(0, word.Length, $"Test {i}");
+        }
+    }
+}
